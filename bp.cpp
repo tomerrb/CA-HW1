18dfe6b9da22cf6a646c760de1008b903fc9880e
Gil Kapel <gil.kapel@gmail.com>
2021-11-14 22:35:21 +0200

    function 1,3,4 and tones of small changes! (don't have good excuses)

--- bp.cpp
+++ bp.cpp
@@ -58,28 +58,45 @@ class fsm
 private:
 	states* FSMs;
 	int fsm_num;
+	states fsm_state;
 
 public:
-	fsm(states* fsms, int num_of_fsm);
+	fsm(states* fsms, int num_of_fsm, states initial_state);
 	~fsm()= default;
 	const states* getFSM ();
-	void updateFSM (states* new_fsm);
+	void updateFSM (bool taken);
 	const int getNumOfFSM ();
-	void updateFSMNum (int new_num);
 };
 
-fsm::fsm(states* fsms, int num_of_fsm) : FSMs(fsms), fsm_num(num_of_fsm)
-{
-}
+fsm::fsm(states* fsms, int num_of_fsm) : FSMs(fsms), fsm_num(num_of_fsm), initial_state(WNT){}
 
 const states* fsm::getFSM()
 {
 	return this->FSMs;
 }
 
-void fsm::updateFSM(states* new_fsm)
+void fsm::updateFSM(bool taken)
 {
-	this->FSMs = new_fsm;
+	if(fsm_state == SNT)
+	{
+		if(taken) fsm_state = WNT;
+	}
+	if (fsm_state == WNT)
+	{
+		if(taken) fsm_state = WT;
+		else fsm_state = SNT;
+	}
+	
+	if (fsm_state == WT)
+	{
+		if(taken) fsm_state = ST;
+		else fsm_state = WNT;
+	}
+	
+	if (fsm_state == ST)
+	{
+		if(!taken) fsm_state = WT;
+	}
 }
 
 const int fsm::getNumOfFSM()
@@ -118,7 +135,7 @@ public:
 	const bool getPred ();
 	void updatePred (const bool new_pred);
 	const fsm* getFSM ();
-	void updateFSM(fsm* new_FSM);
+	void updateFSM(bool taken);
 };
 
 branch::branch(uint32_t branchPC, uint32_t targetPC, history* hist, bool pred,  fsm* fsm) : branchPC(branchPC),
@@ -171,9 +188,9 @@ const fsm* branch::getFSM()
 	return this->FSM;
 }
 
-void branch::updateFSM(fsm* new_fsm)
+void branch::updateFSM(bool taken)
 {
-	this->FSM = new_fsm;
+	FSM->updateFSM(taken);
 }
 
 
@@ -186,8 +203,7 @@ class btb
 {
 private:
 	branch* branch_arr;
-	int branches_Num;
-
+	unsigned int branches_Num;
 public:
 	btb(branch* b, int branch_num);
 	~btb() = default;
@@ -195,11 +211,12 @@ public:
 	void updateBranch(branch* new_branch_arr);
 	const int getBranchesNum();
 	void updateBranchesNum(const int new_num);
+	void addNewBranch(uint32_t pc, uint32_t targetPc, bool taken);
+	bool nextPred(uint32_t pc);
+
 };
 
-btb::btb(branch* b, int branch_num): branch_arr(b), branches_Num(branch_num)
-{
-}
+btb::btb(branch* b, int branch_num): branch_arr(b), branches_Num(branch_num){}
 
 const branch* btb::getBranches()
 {
@@ -208,7 +225,7 @@ const branch* btb::getBranches()
 
 void btb::updateBranch(branch* new_branch_arr)
 {
-	this->branch_arr =new_branch_arr; 
+	this->branch_arr = new_branch_arr; 
 }
 
 const int btb::getBranchesNum()
@@ -220,6 +237,36 @@ void btb::updateBranchesNum(const int new_num)
 {
 	this->branches_Num = new_num;
 }
+
+void btb::addNewBranch(uint32_t pc, uint32_t targetPc, bool taken)
+{
+	for(int i = 0 ; i < branches_Num ; i++)
+	{
+		if (branch_arr[i].branchPc == pc)
+		{
+			branch_arr[i].targetPC = targetPc; //****************not sure!!!! update the old targetPc to the new one? 
+			branch_arr[i].updateFSM(taken); //* update the state machine of the existing pc branch
+		}
+	}
+	if(not enough space in the array) allocate more;
+	branch_arr[branches_Num] = new branch(pc, targetPc);
+	branches_Num++;
+
+}
+
+bool btb::nextPred(uint32_t pc)
+{
+	for (int i = 0 ; i < branches_Num ; i++)
+	{
+		if (branch_arr[i].branchPc == pc)
+		{
+			if(branch_arr[i].FSM.fsm_state == SNT || branch_arr[i].FSM.fsm_state == WNT) return false;
+			else return true;
+		}
+	}
+	return false;
+}
+
 
 /**
  * bp class
@@ -236,17 +283,50 @@ private:
 	bool isGlobalHist;
 	bool isGlobalTable;
 	int Shared;
+	SIM_stats bp_stats;
 
 public:
 	bp(btb* BTB, unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned FSMState, bool isGlobalHist, bool isGlobalTable, int shared);
 	~bp() = default;
+	friend int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
+			bool isGlobalHist, bool isGlobalTable, int Shared);
+	friend bool BP_predict(uint32_t pc, uint32_t *dst);
+	friend void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst);
+	friend void BP_GetStats(SIM_stats *curStats);
+	int calculateMemorySize();
+	void statsUpdate(bool taken);
 };
 
 bp::bp(btb* BTB, unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned FSMState, bool isGlobalHist, bool isGlobalTable, int shared):
-		btb(BTB), btbSize(btbSize), historySize(historySize), tagSize(tagSize), fsmState(FSMState), isGlobalHist(isGlobalHist), isGlobalTable(isGlobalTable), shared(shared)
-{
-}
+		btb(BTB), btbSize(btbSize), historySize(historySize), tagSize(tagSize), fsmState(FSMState),
+		isGlobalHist(isGlobalHist), isGlobalTable(isGlobalTable), shared(shared){}
 
+bool IsdataValid(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState)
+	{
+		if(historySize < 1 || historySize > 8) return false;
+
+		if(tagSize > 30 - btbSize || tagSize < 0) return false;
+
+		if(fsmState > 3) return false;
+
+		for(int i = 0 ; i < 6 ; i++)
+		{
+			if (btbSize == 2^i) return true;
+		}
+
+		return false;
+	}
+
+
+void bp::statsUpdate(bool taken)
+{
+	if(!taken)
+	{
+		bp_stats.flush_num += 3; //* update flush number only if necessary *//
+	}
+	bp_stats.br_num++;
+	bp_stats.size = some number we need to update....;
+}
 
 /**
  * hw functions
@@ -254,7 +334,17 @@ bp::bp(btb* BTB, unsigned btbSize, unsigned historySize, unsigned tagSize, unsig
  **/
 int BP_init(unsigned btbSize, unsigned historySize, unsigned tagSize, unsigned fsmState,
 			bool isGlobalHist, bool isGlobalTable, int Shared){
-	return -1;
+	if(IsdataValid(btbSize, historySize, tagSize, fsmState, isGlobalHist, isGlobalTable, Shared)) return -1;
+	
+	this->btb = new btb;
+	this->btbSize = btbSize;
+	this->historySize = historySize;
+	this->tagSize = tagSize;
+	this->fsmState = fsmState;
+	this->isGlobalHist = isGlobalHist;
+	this->isGlobalTable = isGlobalTable;
+	this->Shared = Shared;
+	return 0;
 }
 
 bool BP_predict(uint32_t pc, uint32_t *dst){
@@ -262,23 +352,22 @@ bool BP_predict(uint32_t pc, uint32_t *dst){
 }
 
 void BP_update(uint32_t pc, uint32_t targetPc, bool taken, uint32_t pred_dst){
-	this->addNewNoteToBTB(pc, targetPc, taken); // add new note to the BTB 
-	if(this->nextPred(taken)){  // changes the SM and returns if the prediction is taken or not
-		this->changePc(pc,pred_dst); //change the current pc of the predictor
+	if(this->btb->nextPred(pc)){  // check if the pc state machine shows to take the prediction or not
+		this->btb->addNewBranch(pc, pred_dst, taken); // add new noot to thr btb - add the pred_dst because the prediction is taken
 		this->statsUpdate(false); // change the SIM_stats saved in the predictor - add 1 to br_num, "false" = don't add any flush_num
 	}
 	else{
-		this->changePc(pc,targetPc);
+		this->btb->addNewBranch(pc, targetPc, taken); // add new note to the BTB - add the targetPC because the prediction is not taken
 		this->statsUpdate(true); // change the SIM_stats saved in the predictor - add 1 to br_num, "true" = add 3 to flush_num
 	}
 	return;
 }
 
 void BP_GetStats(SIM_stats *curStats){
-	curStats->br_num = this->getBrNum();
+	curStats->br_num = this->btb->getBranchesNum();
 	curStats->flush_num = this->getFlushNum();
-	curStats->size = this->calculateSize();
-	delete(this);
+	curStats->size = this->calculateMemorySize();
+	delete(something!!!!!!!!!);
 }
 	return;
 }
